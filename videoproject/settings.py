f2993881ff9aa59f8f8e3683f6855d481b357b16
janyroo <214778990@qq.com>
2023-03-25 13:04:56 +0800

    web05

--- videoproject/settings.py
+++ videoproject/settings.py
@@ -150,8 +150,10 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "images"),
+]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static/")
